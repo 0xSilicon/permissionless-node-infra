45b324ea38033bc2e54034337f9d5a5538654eac
deki <deki@ozys.net>
2025-03-18 16:02:51 +0900

    snapshot restore scripts

--- modules/rds/backup/main.tf
+++ modules/rds/backup/main.tf
@@ -27,6 +27,24 @@ data "terraform_remote_state" "ec2_db" {
     profile = var.aws_profile_name
   }
 }
+resource "aws_ssm_document" "stop_docker" {
+  name            = "StopDocker"
+  document_type   = "Command"
+  document_format = "YAML"
+
+  content = <<EOT
+schemaVersion: "2.2"
+description: "Restart Docker containers using SSM"
+mainSteps:
+  - action: "aws:runShellScript"
+    name: "restart_docker_script"
+    inputs:
+      runCommand:
+        - "#!/bin/bash"
+        - "echo Stop Docker containers on $(hostname)..."
+        - "cd /home/ssm-user && sudo docker compose stop"
+EOT
+}
 
 resource "aws_ssm_document" "restore_rds" {
   name            = "RestoreRDS"
@@ -40,39 +58,24 @@ mainSteps:
     name: "restore_rds_script"
     inputs:
       runCommand:
-        - "exec /bin/bash\ncd $HOME"
-        - "sudo apt update -y"
-        - "sudo apt install -y postgresql gzip awscli"
+        - "#!/bin/bash"
+        - "cd /home/ssm-user"
         - "BACKUP_DATE=${var.backup_date}"
         - "S3_BUCKET=chaindata-sepolia.silicon.network"
         - "DB_HOST=${data.terraform_remote_state.rds.outputs.silicon_cluster_info.endpoint[0]}"
         - "DB_USER=${data.terraform_remote_state.rds.outputs.silicon_cluster_info.master_username[0]}"
-        - "PGPASSWORD=${var.master_password} psql -h \"$DB_HOST\" -U \"$DB_USER\" -d state_db -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
-        - "aws s3 cp \"s3://$S3_BUCKET/$BACKUP_DATE.state.sql.gz\" /tmp/$BACKUP_DATE.state.sql.gz"
-        - "gunzip -c /tmp/$BACKUP_DATE.state.sql.gz | PGPASSWORD=${var.master_password} psql -h \"$DB_HOST\" -U \"$DB_USER\" -d state_db"
-        - "PGPASSWORD=${var.master_password} psql -h \"$DB_HOST\" -U \"$DB_USER\" -d prover_db -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;'"
-        - "aws s3 cp \"s3://$S3_BUCKET/$BACKUP_DATE.prover.sql.gz\" /tmp/$BACKUP_DATE.prover.sql.gz"
-        - "gunzip -c /tmp/$BACKUP_DATE.prover.sql.gz | PGPASSWORD=${var.master_password} psql -h \"$DB_HOST\" -U \"$DB_USER\" -d prover_db"
+        - "export PGPASSWORD=${var.master_password}"
+        - "psql -h \"$DB_HOST\" -U \"$DB_USER\" -d state_db -c 'DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA public;'"
+        - "psql -h \"$DB_HOST\" -U \"$DB_USER\" -d state_db -c 'DROP SCHEMA IF EXISTS state CASCADE; CREATE SCHEMA state;'"
+        - "psql -h \"$DB_HOST\" -U \"$DB_USER\" -d prover_db -c 'DROP SCHEMA IF EXISTS state CASCADE; CREATE SCHEMA state;'"
+        - "psql -h \"$DB_HOST\" -U \"$DB_USER\" -d prover_db -c 'DROP SCHEMA IF EXISTS public CASCADE; CREATE SCHEMA public;'"
+        - "echo 'Restoring state_db from S3...'"
+        - "aws s3 cp \"s3://$S3_BUCKET/$BACKUP_DATE.state.sql.gz\" - || { echo 'Error: Backup file not found!' >&2; exit 1; }"
+        - "zcat | psql -h \"$DB_HOST\" -U \"$DB_USER\" -d state_db || { echo 'Error: state_db restore failed!' >&2; exit 1; }"
+        - "echo 'Restoring prover_db from S3...'"
+        - "aws s3 cp \"s3://$S3_BUCKET/$BACKUP_DATE.prover.sql.gz\" - || { echo 'Error: Backup file not found!' >&2; exit 1; }"
+        - "zcat | psql -h \"$DB_HOST\" -U \"$DB_USER\" -d prover_db || { echo 'Error: prover_db restore failed!' >&2; exit 1; }"
 EOT
-}
-
-resource "terraform_data" "restore_rds" {
-  triggers_replace = {
-    timestamp = timestamp() # Apply할 때마다 실행되도록 변경 감지
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-aws ssm send-command \
-  --document-name "${aws_ssm_document.restore_rds.name}" \
-  --targets "Key=instanceIds,Values=${data.terraform_remote_state.ec2_db.outputs.init_rds_instance.id[0]}" \
-  --comment "Restoring RDS via Terraform" \
-  --region ${var.aws_region} \
-  --profile ${var.aws_profile_name} \
-  --output-s3-bucket-name ${var.s3_bucket} \
-  --output-s3-key-prefix "ssm-logs/"
-EOT
-  }
 }
 
 resource "aws_ssm_document" "restart_docker" {
@@ -88,14 +91,55 @@ mainSteps:
     name: "restart_docker_script"
     inputs:
       runCommand:
+        - "#!/bin/bash"
         - "echo Restarting Docker containers on $(hostname)..."
-        - "sudo docker compose up -d"
+        - "cd /home/ssm-user && sudo docker compose up -d --force-recreate"
 EOT
+}
+
+resource "terraform_data" "stop_docker" {
+  triggers_replace = {
+    timestamp = timestamp() # Apply할 때마다 실행되도록 변경 감지
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+aws ssm send-command \
+  --document-name "${aws_ssm_document.stop_docker.name}" \
+  --targets "Key=instanceIds,Values=${join(",", [data.terraform_remote_state.ec2.outputs.executor_instance_info.id[0], data.terraform_remote_state.ec2.outputs.public_rpc_instance_info.id[0]])}" \
+  --comment "Stopping Docker via Terraform" \
+  --region ${var.aws_region} \
+  --profile ${var.aws_profile_name} \
+  --output-s3-bucket-name ${var.s3_bucket} \
+  --output-s3-key-prefix "ssm-logs/" \
+  --output json > stop_docker_result.json
+EOT
+  }
+}
+
+resource "terraform_data" "restore_rds" {
+  triggers_replace = {
+    restore_enabled = var.restore_enabled # 값이 true일 때만 실행
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+aws ssm send-command \
+  --document-name "${aws_ssm_document.restore_rds.name}" \
+  --targets "Key=instanceIds,Values=${data.terraform_remote_state.ec2_db.outputs.init_rds_instance.id[0]}" \
+  --comment "Restoring RDS via Terraform" \
+  --region ${var.aws_region} \
+  --profile ${var.aws_profile_name} \
+  --output-s3-bucket-name ${var.s3_bucket} \
+  --output-s3-key-prefix "ssm-logs/" \
+  --output json > restore_rds_result.json
+EOT
+  }
 }
 
 resource "terraform_data" "restart_docker" {
   triggers_replace = {
-    timestamp = timestamp() # Apply할 때마다 실행되도록 변경 감지
+    timestamp = timestamp()
   }
 
   provisioner "local-exec" {
@@ -107,7 +151,8 @@ aws ssm send-command \
   --region ${var.aws_region} \
   --profile ${var.aws_profile_name} \
   --output-s3-bucket-name ${var.s3_bucket} \
-  --output-s3-key-prefix "ssm-logs/"
+  --output-s3-key-prefix "ssm-logs/" \
+  --output json > restart_docker_result.json
 EOT
   }
 }
